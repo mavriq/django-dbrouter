0233a91bda459468e9a03d2aeb42f1eddef4f5ab
mavriq <admin@mavriq.net>
2022-03-29 14:07:44 +0300

    bump version

--- dbrouter/version.py
+++ dbrouter/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-VERSION = (0, 1, 4)
+VERSION = (0, 1, 4, 'post1')
 __version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -48,6 +48,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Django',
         'Framework :: Django :: 1.5',
         'Framework :: Django :: 1.6',
@@ -61,5 +65,7 @@ setup(
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
     ]
 )
